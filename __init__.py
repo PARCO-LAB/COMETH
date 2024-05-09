ab6b1f28bb9a12f20763527c44ebc8a16642d508
DoctorMartins9 <enrico.martini@univr.it>
2024-05-09 21:03:31 +0200

    Refactoring

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
 # from . import Skeleton, ConstrainedSkeletond
 # from .Skeleton.Skeleton import DynamicSkeleton
-from .BiomechanicalModel import Skeleton, ConstrainedSkeleton
-from .BiomechanicalModel import DynamicSkeleton
+from .COMETH import Skeleton, ConstrainedSkeleton
+from .COMETH import DynamicSkeleton
